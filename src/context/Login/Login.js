941e5540c8be969e43b2b4010b8d603c34d963c0
Primo5 <primocincko@gmail.com>
2022-06-28 13:34:30 -0400

    feacture(common):login design completed[WEBSITESPC-1]

--- src/context/Login/Login.js
+++ src/context/Login/Login.js
@@ -1,21 +1,43 @@
 import React from "react";
+import Vector from './Vector.svg';
 import './Login.css';
+import PropTypes from 'prop-types';
 export const Login = () => {
+    // let eye=document.getElementById('eye');
+    // let password=document.getElementById('password');
+    // let ver=(eye,password)=>{
+    //     if(password.type == "password"){
+    //         password.type="text";
+    //         console.log(password);
+    //         eye.style.opacity=0.8;
+    //     }else{
+    //         password.type = "password"
+    //         eye.style.opacity=0.2;
+    //     }
+    // }
+const [shown, setShown]=React.useState(false);
+const switchShown=()=>setShown(!shown)
+const [password, setPassword] = React.useState('');
+const onChange = ({ currentTarget }) => setPassword(currentTarget.value);
     return (
         <>
         <form className="form">
             <p className="circulo"><p className="centro"></p></p>
             <h1 className="form_h1title">Plagio control</h1>
             <h2 className="form_title">Login Plagio Conltrol</h2>
-            <p className="form_paragraph">Enter your email and password belew</p>
+            <p className="form_paragraph">Enter your email and password below</p>
             <div className="form_container">
                 <div className="form_group">
                     <label for="email" className="form_label">EMAIL</label><br/>
                     <input type="email" id="email" className="form_input" placeholder="Email address"></input>
                 </div>
-                <div className="form_group">
-                    <label for="password" className="form_label">PASSWORD</label><br/>
-                    <input type="password" id="password" className="form_input" placeholder="Password"></input>
+                <div className="form_group2">
+                    <div className="form_coment">
+                        <label for="password" className="form_label">PASSWORD</label><br/>
+                        <a href="#" className="form_forgot" >Forgot password?</a>
+                    </div>
+                    <input id="password" className="form_input" placeholder="Password" onChange={onChange} type={shown ? 'text':'password'} value={password}></input>
+                    <img src={Vector} className="icon" id="eye" onClick={switchShown} ></img>
                 </div>
                 <input type="submit" value="Log In" className="form_submit"></input>
                 <p className="form_paragraph">No tienes una cuenta? <a href="#" className="form_link">Sign up</a></p>

a82d272de0ac62ebbf8b7d918164035fc650ee35
jezpo <jezocam@gmail.com>
2022-06-29 16:52:44 -0400

    "WEBSITEPC-11 (implemented the card route)"

--- src/context/Works/Works.js
+++ src/context/Works/Works.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Card } from "../../ui/components/Card/Card.js";
 export const Works = () => {
     return (
         <div>
             Works page
+            <Card />
         </div>
     )
 }

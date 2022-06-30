8101cae38fb0a97c7e2b505b10eda8059fdc6aa6
jezpo <77103678+jezpo@users.noreply.github.com>
2022-06-30 00:36:13 -0400

    Update Card.js
    to change to styles using material ui of card

--- src/ui/components/Card/Card.js
+++ src/ui/components/Card/Card.js
@@ -1,12 +1,60 @@
 import React from 'react'
-
-export const Card = () => {
+import {Box, Paper, ButtonBase, styled, Typography, Grid } from "@material-ui/core"
+export const Cards = () => {
+  const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '140px',
+    maxHeight: '210px',
+  });
   return (
-    <div>
-        este es un card
-        
-    </div>
-  )
+    <Paper
+    sx={{
+      p: 3,
+      margin: 'auto',
+      maxWidth: '499px',
+      maxHeight: '300px',
+      flexGrow: 1,
+      backgroundColor: (theme) =>
+        theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    }}
+  >
+    <Grid container spacing={2}>
+      
+      <Grid item xs={12} sm container>
+        <Grid item xs container direction="column" spacing={2}>
+          <Grid item xs>
+            <Typography gutterBottom variant="h2" component="div">
+             Autor. Ing. Raul Gomez
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+            Analisis de la investigacion sobre radio en bolivia...
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              322 pages
+            </Typography>
+          </Grid>
+          
+        </Grid>
+        <Grid item>
+          <Typography variant="subtitle1" component="div">
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Box>
+          <Typography hreft="#">ver detales</Typography>
+        <ButtonBase sx={{ width: 128, height: 128 }}>
+          <Img alt="complex" src="https://source.unsplash.com/random" />
+        </ButtonBase>
+        </Box>
+      </Grid>
+    </Grid>
+  </Paper>
+    
+  );
 }
+
+
 
 

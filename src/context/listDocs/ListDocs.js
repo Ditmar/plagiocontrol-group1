b99fe4fac944e4870ef6458269b9620bfef8faaa
Alejandro16MV <02alejandro13mv@gmail.com>
2022-06-30 03:47:15 -0400

    feat(common):Last Documents Storybook

--- src/context/listDocs/ListDocs.js
+++ src/context/listDocs/ListDocs.js
@@ -1,9 +1,44 @@
 import React from "react";
-
+import './CardsDocs.css'
 export  const ListDocs = () => {
     return (
-        <div>
-            List page
+        <div className="docs">
+            <div>
+                <p className="letra1">
+                            Ultimos Documentos ublicados
+                </p>
+                <p className="group">
+                    Group: Support
+                </p>
+                <ul>
+                    <div className="ticket1">
+                        <div>
+                            <p className="letraticket">Analisis de investigacion sobre radio en Bolivia ...</p>
+                        </div>
+                        <div className="details">
+                            <p className="letraview">View details</p>
+                        </div>
+                        <div>
+                            <p className="letrapag">321 Pag.</p>
+                        </div>
+                    </div>
+
+                    <div className="divider"> </div>
+
+                    <div className="ticket2">
+                        <div>
+                            <p className="letraticket">Análisis bibliométrico y de redes sociales en tesis ...</p>
+                            </div>
+                            <div className="details2">
+                                <p className="letraview">View details</p>
+                            </div>
+                            <div>
+                                <p className="letrapag">120 Pag.</p>
+                        </div>
+                    </div>
+                </ul>
+            </div>
         </div>
+        
     )
 }

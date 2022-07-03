e7f97a743c162af6dc80ba87f8624e92fbb657f4
Ditmar <ditmarcastro@gmail.com>
2022-07-02 20:52:05 -0400

    Revert ""WEBSITEPC-11 (implemented the card route)""
    This reverts commit a82d272de0ac62ebbf8b7d918164035fc650ee35.

--- src/context/Works/Works.js
+++ src/context/Works/Works.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Card } from "../../ui/components/Card/Card.js";
+
 export const Works = () => {
     return (
         <div>
             Works page
-            <Card />
         </div>
     )
 }

d0eceea95ea35d7dd692d7f76f1988418d26026e
Byron Buitrago <byjoselsig@gmail.com>
2022-07-05 15:31:34 -0400

    Secundo Commit Header

---
+++ src/pages/PageCTA.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageCTA = () => {
+    return <h1>Perfil</h1>;
+};
+
+export default PageCTA;
---
+++ src/pages/PageTwo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageTwo = () => {
+    return <h1>Notificaciones</h1>;
+};
+
+export default PageTwo;

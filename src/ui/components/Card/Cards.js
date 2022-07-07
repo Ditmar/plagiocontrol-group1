93adce64350f0682856d9a492e63f9b5256f5b66
jezpo <jezocam@gmail.com>
2022-07-07 16:26:49 +0000

    change to image and styles

--- src/ui/components/Card/Cards.js
+++ src/ui/components/Card/Cards.js
@@ -1,31 +1,35 @@
 import { PropTypes } from "prop-types";
 import './Cards.css';
-export function Cards() {
+import book from './Img/cardbook.png';
+export function Cards({label}) {
+  let styles = {
+    fontFamily: 'Arial',
+  }
   return (
+
     <>
     
     <div className="container">
-          <div className="card-container" >
-            <div className="description">
-              <div className="title"><h3>Autor. Ing. Raul Gomez</h3></div>
+          <div className="card-container" style={styles}>
+            <div className="description"style={styles}>
+              <div className="title" style={styles}><h3>Autor. Ing. Raul Gomez</h3></div>
               
               <br/>
               <br/>
               
               
               
-              <div className="grid-container">
-                <div className="grid-item"><p>Analisis de la investigacion sobre radio en bolivia...</p></div>
-                  <div className="grid-item1"><p className="pag">325 pag</p></div>
+              <div className="grid-container"style={styles}>
+                <div className="grid-item"style={styles}><p>Analisis de la investigacion sobre radio en bolivia...</p></div>
+                  <div className="grid-item1"style={styles}><p className="pag">325 pag</p></div>
                 </div>
                 
                 <hr/>
               
             </div>
-            <div className="header">
+            <div className="header"style={styles}>
               <h5 >Ver Detalles</h5>
-              
-                <img src="https://source.unsplash.com/random/140x210" alt="" width={140} height={210} />
+              <img src={book} width={140} height={210}/>
               
             </div>
           </div>

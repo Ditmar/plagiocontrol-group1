6a7f1cdef1e9858dc1a854596008cc8b9ec5c6c5
thalia romay <thaliaromay2502@gmail.com>
2022-07-08 03:36:51 -0400

    Card_Home_Storybook

--- src/ui/components/HomeCard/HomeCard.jsx
+++ src/ui/components/HomeCard/HomeCard.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 import './HomeCard.css';
 import PropTypes from 'prop-types';
 export const HomeCard= ({documentPages,reviewedPages,copyPagesDetected}) => {
     return (
-        <div className=' containerP'>
+        <div className="containerP">
         <div className=" container1 " >
-            <p className='Title1'>Documentos</p> 
-            <section className='number1'>
-            <p>{documentPages || '60'}</p>
+            <p className="Title1">Documentos</p> 
+            <section className="number1">
+            <p>{documentPages || "60"}</p>
             </section>
         </div>
         <div className="container1 " >
-            <p className='Title1'>Revisados</p>
-            <section className='number1'>
-            <p>{reviewedPages || '16'}</p>
+            <p className="Title1">Revisados</p>
+            <section className="number1">
+            <p>{reviewedPages || "16"}</p>
             </section>
         </div>
         <div className=" container1 " >
-            <p className='Title1'>Copias Detectadas</p>
-            <section className='number1'>
-            <p>{copyPagesDetected || '43'}</p>
+            <p className="Title1">Copias Detectadas</p>
+            <section className="number1">
+            <p>{copyPagesDetected || "43"}</p>
             </section>
         </div> 
         </div>

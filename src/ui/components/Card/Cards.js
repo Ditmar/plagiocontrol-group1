3eb6589f56605f7b494ce7b67210baa64c4a56a0
jezpo <jezocam@gmail.com>
2022-07-14 07:20:52 +0000

    incorporate to api from card

--- src/ui/components/Card/Cards.js
+++ src/ui/components/Card/Cards.js
@@ -1,54 +1,55 @@
 import { PropTypes } from "prop-types";
-import './Cards.css';
-import book from './Img/cardbook.png';
-import { useEffect, useState} from 'react';
-export function Cards({label}) {
+import "./Cards.css";
+import { useEffect, useState } from "react";
+export function Cards({ label }) {
   let styles = {
-    fontFamily: 'Arial',
-  }
-  const url = 'http://3.138.158.90:8000/server/listdatabase';
-  const [todos, setTodos] = useState();
-  const fetchApi = async () => {
-    const response = await fetch(url)
-    console.log(response.status)
-    const responseJSON = await response.json()
-    setTodos(responseJSON)
-    console.log(responseJSON)
-  }
-  useEffect(()=>{
-    fetchApi()
-  },[])
-  return (
+    fontFamily: "Arial",
+  };
 
-    <>
+  const [users, setUsers] = useState();
+  useEffect(() => {
+    const getUsers = async () => {
+      const id = sessionStorage.getItem("idUsuario");
+      const token = sessionStorage.getItem("token");
+      const res = await fetch(
+        `https://backendsingin.herokuapp.com/user/listarUsersCurso/` + id,
+        {
+          method: "GET",
+          headers: { autorizacion: token },
+        }
+      );
     
+      const response = await res.json();
+      setUsers(response);
+    };
+    getUsers();
+  }, []);
+  return (
     <div className="container">
+      
+        {users?.map((user) => (
           <div className="card-container" style={styles}>
-            <div className="description"style={styles}>
-              <div className="title" style={styles}><h3>Autor. Ing. Raul Gomez</h3></div>
-              
-              <br/>
-              <br/>
-              
-              
-              
-              <div className="grid-container"style={styles}>
-                <div className="grid-item"style={styles}><p>Analisis de la investigacion sobre radio en bolivia...</p></div>
-                  <div className="grid-item1"style={styles}><p className="pag">325 pag</p></div>
-                </div>
-                
-                <hr/>
-              
-            </div>
-            <div className="header"style={styles}>
-              <h5 >Ver Detalles</h5>
-              <img src={book} width={140} height={210}/>
-              
-            </div>
-          </div>
-        </div>
+          <div className="description" style={styles}>
+              <div className="title" style={styles}>  <h3>{user.autor}</h3></div>
 
-        </>  
+               <br />
+                <br />
+
+               <div className="grid-container" style={styles}>
+                  <div className="grid-item" style={styles}><p>{user.name}</p></div>
+                  <div className="grid-item1" style={styles}>   <p className="pag">325 pag</p></div>
+               </div>
+              <hr />
+              </div>
+                 <div className="header" style={styles}>
+                  <h5>Ver Detalles</h5>
+                  <img src={user.avatar} width={140} height={210} />
+                 </div>
+             
+            </div>
+        ))}
+      
+    </div>
   );
 }
 

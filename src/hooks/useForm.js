091a0e102f90f19d641c0338b9a2df13c9cc902d
bryancruzn <103218584+bryancruzn@users.noreply.github.com>
2022-07-15 01:44:36 -0400

    fix the branch conflicts

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,13 +2,9 @@ import { useState } from "react";
 
 const useForm = (initForm) => {
     const [form, setForm] = useState(initForm);
-
-    ///////////
     const handlerChangeForm = ({ target }) => {
         setForm({...form, [target.name]: target.value});
     }
-
-    ////////
     const handlerResetForm = (setDataToEdit) => {
         setForm(initForm);
         setDataToEdit(null);
